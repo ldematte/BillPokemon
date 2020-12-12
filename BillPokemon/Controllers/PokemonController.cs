a0e06c001954d71bd67372f1cbb07b7fbc38c073
ldematte <lorenzo.dematte@gmail.com>
2020-12-12 15:43:28 +0100

    Initial commit: skeleton project/solution with swagger and Dokerfile

---
+++ BillPokemon/Controllers/PokemonController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BillPokemon.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PokemonController : ControllerBase
+    {
+        private readonly ILogger<PokemonController> m_logger;
+
+        public PokemonController(ILogger<PokemonController> mLogger)
+        {
+            m_logger = mLogger;
+        }
+
+        [HttpGet]
+        public PokemonDescription Get(string name)
+        {
+            return new PokemonDescription
+            {
+                Name = name,
+                Description = "Description"
+            };
+        }
+    }
+}
---
+++ BillPokemon/PokemonDescription.cs
@@ -0,0 +1,8 @@
+namespace BillPokemon
+{
+    public class PokemonDescription
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
